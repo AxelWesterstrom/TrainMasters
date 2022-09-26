2427cee57d297c1035fc50f02f2833940a5336aa
Axel Westerstrom <axel.westerstrom@gmail.com>
2022-09-26 21:49:37 +0200

    ticket page

--- src/pages/Ticket.jsx
+++ src/pages/Ticket.jsx
@@ -1,30 +1,27 @@
 import Header from "../components/Header";
+import TicketTemplate from "../components/TicketTemplate";
 import React from "react";
-import styles from "../../public/css/home.css";
+import styles from "../../public/css/ticket.css";
+
 import { Row, Col, Button, Form, Container } from "react-bootstrap";
 
 
 function Ticket() {
   return (
     <>
-      <div className="ticket">
-        <div className="header">
-          <Header />
-        </div>
-        <div className="main">
-          <div className="d-flex justify-content-center mt-5 pt-5">
-            <Container className="p-1 m-1">
-              <Container className="ticketBorder">
-                <Row ></Row>
-                <Row>
-                  <Col>Biljett</Col>
-                </Row>
-              </Container>
-            </Container>
-          </div>
-        </div>
+      <Row className="header">
+        <Header />
+      </Row>
+      <Row>
+        <TicketTemplate _ />
+      </Row>
+      <div className="d-flex justify-content-center">
+        <Button className="ticketButton" type="submit">
+          Fortsätt
+        </Button>
       </div>
     </>
+
   );
 }
 export default Ticket;
