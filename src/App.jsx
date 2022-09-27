8a308f46bbeb6250f569655f279f6602cc5692a5
Shana(Sana) <sanabarilad@gmail.com>
2022-09-27 09:04:07 +0200

    fixed a confilct

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,15 @@ import Ticket from "./pages/Ticket";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/valj-resa' /> //we have route path in swedish like SJ's
-          website
-          <Route path='/valj-tag' element={<PickJourney />} /> //There is a
-          header component and we can use it for all other pages
-          <Route path='/anpassa-resa' />
-          <Route path='/kassan' />
-          <Route path='/mina-biljetter' element={<Ticket />} />
-          <Route path='/valj-tag' element={<PickJourney />} /> //There is a
-          header component and we can use it for all other pages
-           <Route path="/anpassa-resa" element={<CustomizeTrip />} />
-          <Route path='/kassan' />
-          <Route path='/mina-biljetter' element={<Ticket />} />
-
+          <Route path="/" element={<Home />} />
+          <Route path="/valj-resa" />
+          <Route path="/valj-tag" element={<PickJourney />} />
+          <Route path="/anpassa-resa" element={<CustomizeTrip />} />
+          <Route path="/kassan" />
+          <Route path="/mina-biljetter" element={<Ticket />} />
         </Routes>
       </BrowserRouter>
     </div>
