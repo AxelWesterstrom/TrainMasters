08a79d56bae6fcc9797805fc1dbebf3ff9f0617c
MergimJakupi <baka.shiro20@gmail.com>
2022-10-03 23:09:11 +0200

    wip

---
+++ src/pages/Payment.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header";
+import React from "react";
+import { Row, Col, Button, Form, Container } from "react-bootstrap";
+
+
+function Payment() {
+  return (
+    <>
+      <Row className="header">
+        <Header/>
+      </Row>
+    </>
+  );
+}
+
+export default Payment;
