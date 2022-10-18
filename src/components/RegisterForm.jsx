c9ce96a701b702bd7c88679ea0bebeab5040c0b5
Axel Westerstrom <axel.westerstrom@gmail.com>
2022-10-18 19:49:49 +0200

    popup when logging in

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { useStates } from "../assets/helpers/states";
 import Header from "../components/Header";
 
+
 function RegisterForm() {
 
   const navigate = useNavigate();
@@ -13,10 +14,16 @@ function RegisterForm() {
   let u = useStates("user");
 
   let l = useStates({
+    firstName: "",
+    lastName: "",
     email: "",
     password: "",
     passwordCheck: ""
   });
+
+  let customerToRegister = { email: l.email, password: l.password, personId: 0 }
+  let personToRegister = { firstName: l.firstName, lastName: l.lastName }
+
   const handleClose = () => setShow(false);
   const [show, setShow] = useState();
 
@@ -28,8 +35,9 @@ function RegisterForm() {
   }
 
   async function registerAttempt() {
+    console.log(l);
     let email = await (await fetch(`/api/customers?email=${l.email}`)).json();
-    if (l.email.length == 0 || l.password.length == 0 || l.passwordCheck.length == 0) {
+    if (l.email.length == 0 || l.password.length == 0 || l.passwordCheck.length == 0 || l.lastName == 0 || l.firstName == 0) {
       setErrorMessage("Alla fält måste fyllas i");
       setShow(true);
     }
@@ -46,9 +54,27 @@ function RegisterForm() {
     }
   }
 
+  async function addToDatabase() {
+    let { insertId } = await (await fetch(`/api/people`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(personToRegister),
+    })).json();
+    customerToRegister.personId = insertId;
+
+    let { insertId: customerId } = await (await fetch(`/api/customers`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(customerToRegister),
+    })).json();
+    u.customerId = customerId;
+  }
+
   function addUser() {
     log.login = "true";
     u.email = l.email;
+    u.showMessage = 'register';
+    addToDatabase();
     navigate("/");
   }
 
@@ -66,6 +92,10 @@ function RegisterForm() {
                 <Col className=" login-form-col">
                   <Form.Group className="login-form">
                     <Form onSubmit={register} autoComplete="off">
+                      <FormLabel className="login-label">Förnamn </FormLabel>
+                      <FormControl type="text" {...l.bind("firstName")} />
+                      <FormLabel className="login-label">Efternamn </FormLabel>
+                      <FormControl type="text" {...l.bind("lastName")} />
                       <FormLabel className="login-label">E-postadress </FormLabel>
                       <FormControl type="email" {...l.bind("email")} />
                       <FormLabel className="login-label">Lösenord </FormLabel>
