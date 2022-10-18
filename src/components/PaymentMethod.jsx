e0e915d4b9d151163043d7c017572f6480a3e9ad
MergimJakupi <baka.shiro20@gmail.com>
2022-10-18 09:20:49 +0200

    responsiveness, radiobutton redo, functionality

--- src/components/PaymentMethod.jsx
+++ src/components/PaymentMethod.jsx
@@ -1,8 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Container, Form, Button } from "react-bootstrap";
+import { useStates } from "../assets/helpers/states";
+import { useState } from "react";
+
+
 
 function PaymentMethod() {
+  const [payment, setPayment] = useState();
+
+  function paymentPopup() {
+
+    if ({ payment } == "swish") {
+      console.log("im SWISH WOOO")
+      console.log({ payment })
+    }
+    else if ({ payment } == "kort") {
+      console.log("im KORT WOOO")
+      console.log({ payment })
+    }
+    else {
+      
+      return (
+        alert("Välj betalsätt!  " + {payment})
+        
+      );
+    }
+  }
+
   return (
     <>
       <Container className="travelerContainer">
@@ -14,29 +39,66 @@ function PaymentMethod() {
         <Form className="paymentMethodForm">
           <Col>
             <Row>
-              {["radio"].map((type) => (
+              {/* {["radio"].map((type) => (
                 <div key={`${type}`} className="mb-3 custom-label">
                   <Form.Check
                     label="Swish"
-                    name="group1"
+                    name="paymentChoice"
                     type={type}
-                    onChange={(e) => setSwishPayment(e.target.value)}
+                    onChange={e=>setPayment(e.target.value)}
                     id={`${type}-swish`}
                   />
                   <Form.Check
                     label="Kort"
-                    name="group1"
+                    name="paymentChoice"
                     type={type}
-                    onChange={(e) => setCardPayment(e.target.value)}
+                    onChange={e=>setPayment(e.target.value)}
                     id={`${type}-kort`}
                   />
                 </div>
-              ))}
+              ))} */}
+              <Row className="d-flex p-2">
+                <Col>
+                  <input
+                    type="radio"
+                    className="custom-radio"
+                    name="paymentChoice"
+                    value="swish"
+                    id="radio-swish"
+                    onChange={(e) => setPayment(e.target.value)}
+                  />
+                </Col>
+                <Col>
+                  <label for="radio-swish">Swish</label>
+                </Col>
+              </Row>
+              <Row className="d-flex p-2">
+                <Col>
+                  <input
+                    type="radio"
+                    className="custom-radio"
+                    name="paymentChoice"
+                    value="kort"
+                    id="radio-kort"
+                    onChange={(e) => setPayment(e.target.value)}
+                  />
+                </Col>
+                <Col>
+                  <label for="radio-kort">Kort</label>
+                </Col>
+              </Row>
+            </Row>
+            <Row>
+              <Button
+                className="custom-button paymentButton"
+                type="button"
+                onClick={paymentPopup}
+              >
+                Fortsätt
+              </Button>
             </Row>
           </Col>
-            <Button className="custom-button paymentButton" type="submit">
-              Fortsätt
-            </Button>
+          <h1>{payment}</h1>
         </Form>
       </Container>
     </>
