2d6be0f6043ecb3ae48a176d10335935f0bf066a
Axel Westerstrom <axel.westerstrom@gmail.com>
2022-10-18 08:19:10 +0200

    wip

--- src/pages/Ticket.jsx
+++ src/pages/Ticket.jsx
@@ -3,7 +3,7 @@ import TicketTemplate from "../components/TicketTemplate";
 import React from "react";
 import styles from "../../public/css/ticket.css";
 import { useStates } from "../assets/helpers/states";
-import { Row, Form, FormLabel, FormControl } from "react-bootstrap"; 4
+import { Row, Form, FormLabel, FormControl, Col, Container } from "react-bootstrap"; 4
 
 function Ticket() {
   let log = useStates("login");
@@ -12,21 +12,41 @@ function Ticket() {
 
   }
 
+  function typeOutTickets() {
+    const tickets = ["gh", "gl", "lkf", "sadasd"]
+    return tickets.map((ticket) => <TicketTemplate />)
+  }
+
   return (
     <>
       <Row className="header">
         <Header />
       </Row>
-      <Row>
-        {!log.login
-          ? <Form onSubmit={searchBooking} autoComplete="off">
-            <FormLabel className="login-label">Bokningsnummer </FormLabel>
-            <FormControl type="text" />
-          </Form>
-          : Array(3).fill(<TicketTemplate />)
-        }
-      </Row>
+
+      <Container overflow="hidden">
+        <Row >
+          {!log.login
+            ? <Container className="form-body">
+              <Row className="form-booking-number" xs={12} md={10} lg={8}>
+                <Form onSubmit={searchBooking} autoComplete="off">
+                  <FormLabel>
+                    <h1>Skriv in ditt bokningsnummer</h1>
+                  </FormLabel>
+                  <FormControl type="text" />
+                </Form>
+              </Row>
+            </Container>
+            :
+            <Row>
+              <h1 className="booking-number">Bokningsnummer: 321494912489</h1>
+              {typeOutTickets()}
+            </Row>
+
+
+          }
+        </Row>
+      </Container>
     </>
-  );
-}
-export default Ticket;
+    );
+  }
+  export default Ticket;
